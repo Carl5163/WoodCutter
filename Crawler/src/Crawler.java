5741ac8093df2d70f5dbb0fdb4cc825cd1cb357f
Carl <silence.is.pointless@gmail.com>
2016-02-03 13:08:59 -0500

    Signed-off-by: Carl <silence.is.pointless@gmail.com>

--- Crawler/src/Crawler.java
+++ Crawler/src/Crawler.java
@@ -7,26 +7,29 @@ public class Crawler {
 	public static final boolean VERBOSE = true;
 	public static final boolean DEBUG = false;
 	public static final int MAX_DEPTH = 2;
-	public static final boolean SHOW_EMPTY_LINKS = false;
-	
+	public static final boolean SHOW_EMPTY_LINKS = false;	
 
 	private PseudoQueue queue;
 	private boolean stopping = false;
-	
+
+	//******************************************************************
 	public static void main(String[] args) {
 		try {
 			new Crawler();
 		} catch (IOException e) {
-			e.printStackTrace();
+			System.out.println("Bad seed. Please try another URL.\nReason: " + e.getMessage());
 		}
 	}
+	//******************************************************************
 
-
+	//******************************************************************
 	public Crawler() throws IOException {
 		crawl();	
-		System.out.println("Done! Output can be found in emailAddr.txt");
+		System.out.println("Done! Output can be found in \"emailAddr.txt\"");
 	}
-	
+	//******************************************************************
+
+	//******************************************************************
 	public void crawl() throws IOException {
 		
 		System.out.println("Started.\nRunning...");
@@ -48,21 +51,22 @@ public class Crawler {
 					System.out.println("Error in CrawlerThread.run(): IOException. Reason: " + e.getMessage());
 				}
 			}
-	}
-	
-	BufferedWriter writer;
-	try {
-		writer = new BufferedWriter(new FileWriter("Emails.txt"));
-		for(int i = 0; i < queue.realSize(); i++) {
-			queue.get(i).write(writer);
 		}
-		writer.close();
-	} catch (IOException e) {
-		if(DEBUG) {
-			System.out.println("Error in CrawlerThread.run(): IOException. Reason: " + e.getMessage());
+	
+		BufferedWriter writer;
+		try {
+			writer = new BufferedWriter(new FileWriter("Emails.txt"));
+			for(int i = 0; i < queue.realSize(); i++) {
+				queue.get(i).write(writer);
+			}
+			writer.close();
+		} catch (IOException e) {
+			if(DEBUG) {
+				System.out.println("Error in CrawlerThread.run(): IOException. Reason: " + e.getMessage());
+			}
 		}
-	}
 	
 	}
+	//******************************************************************
 	
 }
